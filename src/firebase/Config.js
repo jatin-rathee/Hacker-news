2a8151e8e4daad9657a6dc8778d7ca8f06d912cc
Jatin Rathee <jatinrathee2018@gmail.com>
2020-09-07 00:52:03 +0530

    firebase config

---
+++ src/firebase/Config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDq8wutgO74qBOrDeTS59Rk8UyUw4uTASw",
+  authDomain: "newsy-65982.firebaseapp.com",
+  databaseURL: "https://newsy-65982.firebaseio.com",
+  projectId: "newsy-65982",
+  storageBucket: "newsy-65982.appspot.com",
+  messagingSenderId: "343843227634",
+  appId: "1:343843227634:web:a6af8780b905de96aecd6b",
+};
+
+export default firebaseConfig;
