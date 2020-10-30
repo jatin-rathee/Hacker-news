0533d4b8bdfa71264e1cee8ae885fa8e024aabf4
Jatin Rathee <jatinrathee2018@gmail.com>
2020-10-31 00:47:47 +0530

    Link Item added

--- src/components/Link/LinkItem.jsx
+++ src/components/Link/LinkItem.jsx
@@ -1,10 +1,104 @@
-import React from 'react'
-import { IonCard } from '@ionic/react'
+import React from "react";
+import {
+	IonBadge,
+	IonCard,
+	IonCardContent,
+	IonIcon,
+	IonItem,
+	IonLabel,
+	IonList,
+	IonText,
+} from "@ionic/react";
+import {
+	chatbubbleEllipsesOutline,
+	chevronUpCircleOutline,
+	linkOutline,
+	personCircleOutline,
+	timeOutline,
+} from "ionicons/icons";
+import { getHostName } from "../../helpers/domain";
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-const LinkItem = () => {
-  return (
-    <IonCard />
-  )
-}
+const LinkItem = ({ link, index, showCount, url }) => {
+	return (
+		<IonCard routerLink={url} button>
+			<IonCardContent className="ion-no-padding">
+				<IonList line="none">
+					<IonItem>
+						<IonBadge style={{ verticalAlign: "middle" }} slot="start">
+							{showCount && index}
+						</IonBadge>
+						<IonLabel>
+							<p
+								style={{
+									alignItems: "center",
+									fontSize: ".8rem",
+									fontWeight: "normal",
+								}}
+							>
+								<IonIcon
+									icon={linkOutline}
+									style={{ verticalAlign: "middle" }}
+								/>{" "}
+								<IonText style={{ verticalAlign: "middle" }}>
+									{getHostName(link.url)}
+								</IonText>
+							</p>
 
-export default LinkItem
+							<div className="ion-padding-vertical ion-text-wrap">
+								<strong style={{ fontSize: "1rem" }}>{link.description}</strong>
+							</div>
+
+							<p
+								style={{
+									alignItems: "center",
+									fontSize: ".8rem",
+									fontWeight: "normal",
+								}}
+							>
+								<IonIcon
+									icon={chevronUpCircleOutline}
+									style={{ verticalAlign: "middle" }}
+								/>{" "}
+								<IonText style={{ verticalAlign: "middle" }}>
+									{link.voteCount} points
+								</IonText>
+								{" | "}
+								<IonIcon
+									icon={personCircleOutline}
+									style={{ verticalAlign: "middle" }}
+								/>{" "}
+								<IonText style={{ verticalAlign: "middle" }}>
+									{link.postedBy.name}
+								</IonText>
+								{" | "}
+								<IonIcon
+									icon={timeOutline}
+									style={{ verticalAlign: "middle" }}
+								/>{" "}
+								<IonText style={{ verticalAlign: "middle" }}>
+									{formatDistanceToNow(link.created)}
+								</IonText>
+
+								{link.comments.length > 0 && (
+									<>
+										{" | "}
+										<IonIcon
+											icon={chatbubbleEllipsesOutline}
+											style={{ verticalAlign: "middle" }}
+										/>{" "}
+										<IonText style={{ verticalAlign: "middle" }}>
+											{link.comments.length} comments
+										</IonText>
+									</>
+								)}
+							</p>
+						</IonLabel>
+					</IonItem>
+				</IonList>
+			</IonCardContent>
+		</IonCard>
+	);
+};
+
+export default LinkItem;
--- src/components/Link/LinkList.jsx
+++ src/components/Link/LinkList.jsx
@@ -1,49 +1,47 @@
-import React from 'react'
-import firebase from '../../firebase'
+import React from "react";
+import firebase from "../../firebase";
 
-import LinkItem from './LinkItem'
+import LinkItem from "./LinkItem";
 
 const LinkList = (props) => {
+	const [links, setLinks] = React.useState([]);
+	const isTrending = props.location.pathname.includes("trending");
 
-  const [links, setLinks] = React.useState([])
-  const isTrending = props.location.pathname.includes("trending")
+	React.useEffect(() => {
+		const unsubscribe = getLinks();
+		return () => unsubscribe();
+	}, [isTrending]);
 
-  React.useEffect(() => {
-    const unsubscribe = getLinks()
-    return () => unsubscribe()
-  }, [isTrending])
+	function getLinks() {
+		if (isTrending) {
+			return firebase.db
+				.collection("links")
+				.orderBy("voteCount", "desc")
+				.onSnapshot(handleSnapshot);
+		}
+		return firebase.db
+			.collection("links")
+			.orderBy("created", "desc")
+			.onSnapshot(handleSnapshot);
+	}
 
-  function getLinks() {
-    if (isTrending) {
-      return firebase.db
-        .collection("links")
-        .orderBy("voteCount", "desc")
-        .onSnapshot(handleSnapshot)
-    }
-    return firebase.db
-      .collection("links")
-      .orderBy("created", "desc")
-      .onSnapshot(handleSnapshot)
-  }
+	function handleSnapshot(snapshot) {
+		const links = snapshot.docs.map((doc) => {
+			return {
+				id: doc.id,
+				...doc.data(),
+			};
+		});
+		setLinks(links);
+	}
 
-  function handleSnapshot(snapshot) {
-    const links = snapshot.docs.map(doc => {
-      return {
-        id: doc.id, ...doc.data()
-      }
-    })
-    setLinks(links)
-  }
+	return (
+		<>
+			{links.map((link, index) => (
+				<LinkItem key={link.id} showCount={true} url={`/link/${link.id}`} link={link}/>
+			))}
+		</>
+	);
+};
 
-  return (
-    <>
-      {
-        links.map((link, index) => (
-          <LinkItem key={link.id} />
-        ))
-      }
-    </>
-  )
-}
-
-export default LinkList
+export default LinkList;
